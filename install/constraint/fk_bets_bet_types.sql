4e3137490ab96101a4af6dc37e916409ccac72dd
kamil <kamil98991@gmail.com>
2022-11-18 11:38:56 +0100

    Dodawanie constraint

--- install/constraint/fk_bets_bet_types.sql
+++ install/constraint/fk_bets_bet_types.sql
@@ -1,1 +1,4 @@
+--liquibase formatted sql
+--changeset kjagiello:FK2_BETS failOnError:true stripComments:false splitStatements:true rollbackSplitStatements:true runOnChange:true
+--rollback alter table BETS drop constraint FK2_BETS;
 ALTER TABLE BETS ADD CONSTRAINT FK2_BETS FOREIGN KEY (BET_TYPE_ID) REFERENCES BET_TYPES (ID);
--- install/constraint/fk_programs_models.sql
+++ install/constraint/fk_programs_models.sql
@@ -1,1 +1,4 @@
+--liquibase formatted sql
+--changeset kjagiello:FK1_PROGRAMS failOnError:true stripComments:false splitStatements:true rollbackSplitStatements:true runOnChange:true
+--rollback alter table PROGRAMS drop constraint FK1_PROGRAMS;
 ALTER TABLE PROGRAMS ADD CONSTRAINT FK1_PROGRAMS FOREIGN KEY (MODEL_ID) REFERENCES MODELS (ID);
