ded9cc0b2065a0b5c66bc93f496bda2939dbb276
KKAZALA <k.kazala@wit.edu.pl>
2022-11-26 17:50:01 +0100

    Modyfikacja tabel, zasilenie sports_leagues

--- install/constraint/fk_bets_sports_leagues.sql
+++ install/constraint/fk_bets_sports_leagues.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
 --changeset kjagiello:FK1_BETS failOnError:true stripComments:false splitStatements:true rollbackSplitStatements:true runOnChange:true
 --rollback alter table BETS drop constraint FK1_BETS;
-ALTER TABLE BETS ADD CONSTRAINT FK1_BETS FOREIGN KEY (ORGANIZATION_ID) REFERENCES SPORTS_ORGANIZATIONS (ID);
+ALTER TABLE BETS ADD CONSTRAINT FK1_BETS FOREIGN KEY (ORGANIZATION_ID) REFERENCES SPORTS_LEAGUES (ID);
--- install/constraint/fk_programs_sports_leagues.sql
+++ install/constraint/fk_programs_sports_leagues.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
 --changeset kjagiello:FK2_PROGRAMS failOnError:true stripComments:false splitStatements:true rollbackSplitStatements:true runOnChange:true
 --rollback alter table PROGRAMS drop constraint FK2_PROGRAMS;
-ALTER TABLE PROGRAMS ADD CONSTRAINT FK2_PROGRAMS FOREIGN KEY (ORGANIZATION_ID) REFERENCES SPORTS_ORGANIZATIONS (ID);
+ALTER TABLE PROGRAMS ADD CONSTRAINT FK2_PROGRAMS FOREIGN KEY (ORGANIZATION_ID) REFERENCES SPORTS_LEAGUES (ID);
