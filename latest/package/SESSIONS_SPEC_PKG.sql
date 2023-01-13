48e5b2135fdb5d998d47761db6e193bb2079b087
KKAZALA <k.kazala@wit.edu.pl>
2023-01-13 13:36:58 +0100

    Dużo zmian

--- latest/package/SESSIONS_SPEC_PKG.sql
+++ latest/package/SESSIONS_SPEC_PKG.sql
@@ -2,9 +2,7 @@
 --changeset kkazala:SESSIONS_SPEC_PKG failOnError:true stripComments:false splitStatements:true rollbackSplitStatements:true runOnChange:true context:pakiety
 --comment dodanie spec pakietu do rozliczania sesji
 --rollback select 1 from dual;
-CREATE OR REPLACE PACKAGE SESSIONS_SPEC_PKG AS
+CREATE OR REPLACE PACKAGE SESSIONS_BODY_PKG AS
   PROCEDURE CALCULATE_SALES_COMMISSIONS (
     in_program_id IN NUMBER);
-
-  PROCEDURE INSERT_MODEL;
-END SESSIONS_SPEC_PKG;
+END SESSIONS_BODY_PKG;
