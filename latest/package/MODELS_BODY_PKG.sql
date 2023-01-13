70b13a31a2e28f3470881c752357ebeecb369ac9
KKAZALA <k.kazala@wit.edu.pl>
2023-01-13 13:36:58 +0100

    Naprawa algorytmu liczącego prowizję. Zmiana struktury tabelki teams.

--- latest/package/MODELS_BODY_PKG.sql
+++ latest/package/MODELS_BODY_PKG.sql
@@ -15,19 +15,35 @@ CREATE OR REPLACE PACKAGE BODY MODELS_BODY_PKG AS
     END LOOP;
 
     EXECUTE IMMEDIATE l_formula;
-  EXCEPTION WHEN OTHERS THEN
-    raise_application_error (-20001, chr(10)||' Failed: ' || l_formula || ' ' || SQLERRM);
+  EXCEPTION
+    WHEN OTHERS THEN raise_application_error (-20001, 'Nieprawidłowa formuła.');
+    --WHEN OTHERS THEN raise_application_error (-20002, chr(10)||' Failed: ' || l_formula || ' ' || SQLERRM);
   END;
 
   PROCEDURE INSERT_MODEL
   AS
     l_models_row WKSP_CHARLIEDEV.MODELS%ROWTYPE;
   BEGIN
-    l_models_row.NAME := v('P11_NAME');
-    l_models_row.FORMULA := REGEXP_REPLACE(v('P11_FORMULA'), '([[:digit:]|\.])[[:space:]]*', '\1');
+    l_models_row.NAME := v('P' || v('APP_PAGE_ID') || '_NAME');
+    l_models_row.FORMULA := REGEXP_REPLACE(v('P' || v('APP_PAGE_ID') || '_FORMULA'), '([[:digit:]|\.])[[:space:]]*', '\1');
     VALIDATE_FORMULA(l_models_row.FORMULA);
 
     INSERT INTO WKSP_CHARLIEDEV.MODELS VALUES l_models_row;
+    COMMIT;
+  END;
+
+  PROCEDURE EDIT_MODEL
+  AS
+    l_models_row WKSP_CHARLIEDEV.MODELS%ROWTYPE;
+  BEGIN
+    l_models_row.NAME := v('P' || v('APP_PAGE_ID') || '_NAME');
+    l_models_row.FORMULA := REGEXP_REPLACE(v('P' || v('APP_PAGE_ID') || '_FORMULA'), '([[:digit:]|\.])[[:space:]]*', '\1');
+    VALIDATE_FORMULA(l_models_row.FORMULA);
+
+    UPDATE WKSP_CHARLIEDEV.MODELS
+      SET NAME = l_models_row.NAME,
+          FORMULA = l_models_row.FORMULA
+      WHERE ID = v('P' || v('APP_PAGE_ID') || '_ID');
     COMMIT;
   END;
 END MODELS_BODY_PKG;
--- latest/package/SESSIONS_BODY_PKG.sql
+++ latest/package/SESSIONS_BODY_PKG.sql
@@ -7,20 +7,24 @@ CREATE OR REPLACE PACKAGE BODY SESSIONS_BODY_PKG AS
     in_program_id IN NUMBER
   ) AS
     l_programs_row WKSP_CHARLIEDEV.PROGRAMS%ROWTYPE;
-    l_formula WKSP_CHARLIEDEV.MODLES.FORMULA%TYPE;
+    l_formula WKSP_CHARLIEDEV.MODELS.FORMULA%TYPE;
     l_result WKSP_CHARLIEDEV.CALCULATED_SALES_COMMISSIONS.CALCULATED_AMOUNT%TYPE;
   BEGIN
     SELECT * INTO l_programs_row FROM WKSP_CHARLIEDEV.PROGRAMS WHERE ID = in_program_id;
     SELECT FORMULA INTO l_formula FROM WKSP_CHARLIEDEV.MODELS WHERE ID = l_programs_row.MODEL_ID;
 
-    l_formula := 'SELECT ' || l_formula || ' FROM COMMISSION_DATA_V WHERE SPORTS_LEAGUE_ID = ' || l_programs_row.ORGANIZATION_ID || ' AND BET_DATE BETWEEN ' || l_programs_row.DATE_FROM || ' AND ' || l_programs_row.DATE_TO;
+    l_formula := 'SELECT SUM( ' || l_formula || ' ) FROM COMMISSION_DATA_V WHERE SPORTS_LEAGUE_ID = ' || l_programs_row.ORGANIZATION_ID || ' AND BET_DATE BETWEEN '
+                   || 'TO_DATE(''' || l_programs_row.DATE_FROM ||''',''MM/DD/YYYY'')' || ' AND ' || 'TO_DATE(''' || l_programs_row.DATE_TO || ''', ''MM/DD/YYYY'')';
 
-    WHILE LENGTH(l_formula) <70000
-    LOOP
-      l_formula := l_formula || chr(10) || '--comment';
-    END LOOP;
 
-    EXECUTE IMMEDIATE l_formula;
+    --|| 'date'''||to_char(TO_DATE(l_programs_row.DATE_FROM,'MM/DD/YYYY'), 'YYYY-MM-DD')'''
+    --WHILE LENGTH(l_formula) < 70000
+    --LOOP
+    --  l_formula := l_formula || chr(10) || '--comment';
+    --END LOOP;
+    APEX_UTIL.SET_SESSION_STATE('P30_RESULT', l_formula);
+    EXECUTE IMMEDIATE l_formula INTO l_result;
+    APEX_UTIL.SET_SESSION_STATE('P30_RESULT', l_result);
   EXCEPTION WHEN OTHERS THEN
     raise_application_error (-20001, chr(10)||' Failed: ' || l_formula || ' ' || SQLERRM);
   END;
