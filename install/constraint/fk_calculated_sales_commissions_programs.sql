c316b385a500a264e9ff53e6f62a9afbfb678488
KKAZALA <k.kazala@wit.edu.pl>
2023-01-20 13:27:49 +0100

    Usunięcie niepotrzebnego klucza obcego.

--- install/constraint/fk_calculated_sales_commissions_programs.sql
+++
@@ -1,4 +0,0 @@
---liquibase formatted sql
---changeset kjagiello:FK1_CALCULATED_SALES_COMMISSIONS failOnError:true stripComments:false splitStatements:true rollbackSplitStatements:true runOnChange:true
---rollback alter table CALCULATED_SALES_COMMISSIONS drop constraint FK1_CALCULATED_SALES_COMMISSIONS;
-ALTER TABLE CALCULATED_SALES_COMMISSIONS ADD CONSTRAINT FK1_CALCULATED_SALES_COMMISSIONS FOREIGN KEY (PROGRAM_ID) REFERENCES PROGRAMS (ID);
